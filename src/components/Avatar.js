d550f23b19c2c9ea62d8d912436e4b71765c72de
jh k <rlawpgud86@naver.com>
2021-06-03 21:43:41 +0900

    home screen

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,18 +1,19 @@
 import styled from "styled-components";
 
 const SAvatar = styled.div`
-  width: 25px;
-  height: 25px;
-  border-radius: 15px;
+  width: ${(props) => (props.lg ? "30px" : "25px")};
+  height: ${(props) => (props.lg ? "30px" : "25px")};
+  border-radius: 50%;
   background-color: #2c2c2c;
   overflow: hidden;
 `;
 
 const Img = styled.img`
-  max-width: 100%;
+  width: 100%;
+  height: 100%;
 `;
 
-function Avatar({ url = "" }) {
-  return <SAvatar>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
+function Avatar({ url = "", lg = false }) {
+  return <SAvatar lg={lg}>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
 }
 export default Avatar;
