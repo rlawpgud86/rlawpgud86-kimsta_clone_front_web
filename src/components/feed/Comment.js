92d4cdb0d04422d069de3c60778e9add9e6cc824
jh k <rlawpgud86@naver.com>
2021-06-16 21:20:45 +0900

    Comment삭제 기능 준비

---
+++ src/components/feed/Comment.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { FatText } from "../shared";
+import { Link } from "react-router-dom";
+
+const CommentContainer = styled.div`
+  margin-bottom: 7px;
+`;
+const CommentCaption = styled.span`
+  margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+function Comment({ author, payload }) {
+  return (
+    <CommentContainer>
+      <FatText>{author}</FatText>
+      <CommentCaption>
+        {payload.split(" ").map((word, index) =>
+          /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
+    </CommentContainer>
+  );
+}
+
+Comment.propTypes = {
+  author: PropTypes.string.isRequired,
+  payload: PropTypes.string.isRequired,
+};
+
+export default Comment;
+
+//정규표현식--> /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g
